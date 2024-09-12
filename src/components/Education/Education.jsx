647792ddbdcc0f9cb5093816bbcdbfbe3c3cf307
V220404 <sankhevarun22@gmail.com>
2024-09-13 00:02:10 +0530

    first commit

---
+++ src/components/Education/Education.jsx
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import classes from './Education.module.css';
+// import ScrollAnimation from 'react-animate-on-scroll';
+import 'animate.css/animate.min.css';
+import { MdSchool } from 'react-icons/md';
+import { MdWork } from 'react-icons/md';
+import { FaSchool } from 'react-icons/fa';
+
+class Education extends Component {
+  render() {
+    return (
+      <div className={classes.box} id='education'>
+        {/* <ScrollAnimation
+          offset={0}
+          animateIn='fadeInLeft'
+          duration={2}
+          animateOnce={true}
+          initiallyVisible={true}
+        > */}
+        <span className={classes.head}>MY JOURNEY</span>
+        <section className={classes.container}>
+          <div>
+            <div className={classes.row}>
+              <div className={classes.row_md_12}>
+                <div className={classes.timeline_centered}>
+                  {/* <ScrollAnimation
+                      offset={0}
+                      animateIn='fadeInLeft'
+                      duration={2.4}
+                      animateOnce={true}
+                      initiallyVisible={true}
+                    > */}
+                  <article className={classes.timeline_entry}>
+                    <div className={`${classes.timeline_icon} ${classes.timeline_icon_5}`}>
+                      <MdWork />
+                    </div>
+                    <div className={classes.label}>
+                      <h2>
+                        Experience <span>July(2021)-Present</span>
+                      </h2>
+                      <p>
+                        I am currently working as <b>Full Stack APP/Web Developer</b> at{' '}
+                        <a target='_blank' href='https://www.visionxtechnologies.com/'>
+                          <b>VisionX Technologies </b>
+                        </a>{' '}
+                       {' '}
+                        and boosting my professional skills in Real Industry.
+                      </p>
+                    </div>
+                  </article>
+                  {/* </ScrollAnimation> */}
+                  {/* <ScrollAnimation
+                      offset={0}
+                      animateIn='fadeInLeft'
+                      duration={2.4}
+                      animateOnce={true}
+                      initiallyVisible={true}
+                    > */}
+                  <article className={classes.timeline_entry}>
+                    <div className={classes.timeline_icon}>
+                      <MdSchool />
+                    </div>
+                    <div className={classes.label}>
+                      <h2>
+                        Undergraduation at TCET <span>2022-2026</span>
+                      </h2>
+                      <p>
+                        I completing my undergraduation in IT (Information Technology)
+                        from <a href='https://www.tcetmumbai.in/'>TCET</a> 
+                      </p>
+                    </div>
+                  </article>
+                  {/* </ScrollAnimation> */}
+                  {/* <ScrollAnimation
+                      offset={0}
+                      animateIn='fadeInLeft'
+                      duration={2.4}
+                      animateOnce={true}
+                      initiallyVisible={true}
+                    > */}
+                  <article className={classes.timeline_entry}>
+                    <div className={`${classes.timeline_icon} ${classes.timeline_icon_2}`}>
+                      <FaSchool />
+                    </div>
+                    <div className={classes.label}>
+                      <h2>
+                        Higher Education <span>2020-2022</span>
+                      </h2>
+                      <p>
+                        I have completed my higher education from RP Junior College with major subjects as
+                        Physics,Chemistry & Maths with First Class merit in HSC board.{' '}
+                      </p>
+                    </div>
+                  </article>
+                  {/* </ScrollAnimation> */}
+
+                  {/* <ScrollAnimation
+                      offset={0}
+                      animateIn='fadeInLeft'
+                      duration={2.4}
+                      animateOnce={true}
+                      initiallyVisible={true}
+                    > */}
+                  <article>
+                    <div className={`${classes.timeline_icon} ${classes.timeline_icon_4}`}>
+                      <MdWork />
+                    </div>
+                    <div className={classes.label}>
+                      <h2>Internship</h2>
+                      <p>Completed one month Internship in Website Development  with DeltaSoft Systems</p>
+                    </div>
+                    <div className={classes.timeline_entry_inner}>
+                      <div className={classes.timeline_icon_3 || classes.color_none}></div>
+                    </div>
+                  </article>
+                  {/* </ScrollAnimation> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* </ScrollAnimation> */}
+      </div>
+    );
+  }
+}
+
+export default Education;
---
+++ src/components/skills/Interest.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import classes from './Interest.module.css';
+// import ScrollAnimation from 'react-animate-on-scroll';
+import 'animate.css/animate.min.css';
+
+export default function Interest() {
+  return (
+    <div className={classes.box} id='interest'>
+      {/* <ScrollAnimation
+        offset={0}
+        animateIn='fadeInLeft'
+        duration={2.4}
+        animateOnce={true}
+        initiallyVisible={true}
+      > */}
+      <span className={classes.head}>WHAT I DO?</span>
+      <h2 className={classes.heading}>HERE ARE SOME OF MY EXPERTISE</h2>
+      <div className={classes.Interest}>
+        {/* <ScrollAnimation
+            offset={0}
+            animateIn='fadeInLeft'
+            duration={2.4}
+            animateOnce={true}
+            initiallyVisible={true}
+          > */}
+        <div className={classes.web}>
+          <h3>APP Develpoment</h3>
+          <p>
+            I have knowledge of React Native development and have experience in building android and IOS
+            applications. I also have worked on Real Life projects.
+          </p>
+        </div>
+        {/* </ScrollAnimation>
+          <ScrollAnimation
+            offset={0}
+            animateIn='fadeInLeft'
+            duration={2.4}
+            animateOnce={true}
+            initiallyVisible={true}
+          > */}
+        <div className={classes.app}>
+          <h3>Web Development</h3>
+          <p>
+            Javascript, HTML/ CSS, ReactJs, Next.js,  Tailwind , Nodejs , Express
+            CSS, UI Libraries (React-Bootstrap, AceternityUI MUI, Shadcn UI), WordPress , Shopify .
+           
+          </p>
+        </div>
+        {/* </ScrollAnimation>
+          <ScrollAnimation
+            offset={0}
+            animateIn='fadeInLeft'
+            duration={2.4}
+            animateOnce={true}
+            initiallyVisible={true}
+          > */}
+        <div className={classes.other}>
+          <h3>Expanded Expertise</h3>
+          <p>
+            MySQL, MongoDB , Cisco Packet Tracer , VN Editing , AutoCad , Microsoft Excels .
+          </p>
+        </div>
+        {/* </ScrollAnimation> */}
+      </div>
+      {/* </ScrollAnimation> */}
+    </div>
+  );
+}
